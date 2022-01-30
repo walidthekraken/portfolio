497792728029b833363d94f9674545d5d4db6892
doudou820014 <jw_azizi@esi.dz>
2022-01-30 22:56:24 +0100

    some progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,15 @@ import mypic from '../images/pic.jpeg';
 
 export const Home = () => {
     return ( 
-    <div className="Home">
-        <h2>My name is Walid Azizi</h2>
-        <img src={mypic} alt=" pic" width={200} ></img>
+    <div className="home">
+        <div className="hometop">
+            
+            <div>
+                <h2>My name is </h2>
+                <h2 className='myname'> Walid Azizi</h2>
+            </div>
+            <img src={mypic} alt=" pic" width={200} ></img>
+        </div>
 
     </div>
      );
