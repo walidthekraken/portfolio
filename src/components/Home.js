f92fd1032c57fefca2aea15aa18656e8b5173482
doudou820014 <jw_azizi@esi.dz>
2022-01-30 23:04:04 +0100

    some progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,9 @@
 import mypic from '../images/pic.jpeg';
 
 export const Home = () => {
+
+    const job = "Mobile Developer";
+
     return ( 
     <div className="home">
         <div className="hometop">
@@ -9,6 +12,9 @@ export const Home = () => {
             <div>
                 <h2>My name is </h2>
                 <h2 className='myname'> Walid Azizi</h2>
+                <br />
+                <h2>I'm a </h2>
+                <h2 className='myname'> {job}</h2>
             </div>
             <img src={mypic} alt=" pic" width={200} ></img>
         </div>
