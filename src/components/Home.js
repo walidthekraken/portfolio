cb21875d43886f5fe037b6756167166831d6d9b7
doudou820014 <jw_azizi@esi.dz>
2022-01-30 22:17:30 +0100

    some progress

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+
+import mypic from '../images/pic.jpeg';
+
+export const Home = () => {
+    return ( 
+    <div className="Home">
+        <h2>My name is Walid Azizi</h2>
+        <img src={mypic} alt=" pic" width={200} ></img>
+
+    </div>
+     );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <span className="title">Portfolio</span>
+            <div className="links">
+                <a href='/'>Home</a>
+                <span>    </span>
+                <a href='/'>Projects</a>
+                <span>    </span>
+                <a href='/'>Contact</a>
+            </div>
+        </nav>
+    );
+} 
