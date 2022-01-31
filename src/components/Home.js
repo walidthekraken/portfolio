498892720b0e3ebab14f36ec387847d9dde8a79d
doudou820014 <jw_azizi@esi.dz>
2022-01-31 16:02:32 +0100

    some progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,41 @@
 
+import { useEffect,  useState } from 'react';
 import mypic from '../images/pic.jpeg';
 
 export const Home = () => {
 
-    const job = "Mobile Developer";
+    const jobs = ['Mobile Developer', 'Web Developer', 'Data Scientist'];
+    
+
+    const [job, setJob] = useState(jobs[0]);
+    const [ji, setJi] = useState(0);
+    const [letter, setLetter] = useState(jobs[ji].length);
+    
+    useEffect(() => {
+        console.log('im running');
+        let interval = null;
+        const jobs = ['Mobile Developer', 'Web Developer', 'Data Scientist'];
+        
+        interval = setInterval(() => {
+            if (letter>=jobs[ji].length){
+                setTimeout(600);
+                setJi((ji+1)%jobs.length);
+                setJob(jobs[ji]);
+                setLetter((letter+1)%jobs[ji].length)
+                setLetter(1);
+            }else{
+                setLetter(letter+3);
+                setJob(jobs[ji].substring(0,letter));
+            }
+            
+        }, 400);
+        
+
+        return () => clearInterval(interval);
+        
+      }, [job, ji, letter]);
+
+    
 
     return ( 
     <div className="home">
@@ -14,7 +46,7 @@ export const Home = () => {
                 <h2 className='myname'> Walid Azizi</h2>
                 <br />
                 <h2>I'm a </h2>
-                <h2 className='myname'> {job}</h2>
+                <h2 className='job'> {job}</h2>
             </div>
             <img src={mypic} alt=" pic" width={200} ></img>
         </div>
